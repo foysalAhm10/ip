24fc8a17192a5a648a7171abdc5050e1fea77089
Foysal Ahmed <e1333986@u.nus.edu>
2026-01-25 23:08:51 +0800

    Level 2: Add, List

--- src/main/java/FoyBot.java
+++ src/main/java/FoyBot.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FoyBot {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> tasks = new ArrayList<>();
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm FoyBot!\n" +
@@ -18,8 +21,18 @@ public class FoyBot {
                 break;
             }
 
+            if (input.equals("list")) {
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println(i+1 + ". " + tasks.get(i));
+                }
+                System.out.println("    ____________________________________________________________");
+                continue;
+            }
+
             System.out.println("    ____________________________________________________________");
-            System.out.println("    " + input);
+            tasks.add(input);
+            System.out.println("    added: " + input);
             System.out.println("    ____________________________________________________________");
         }
         System.out.println("    ____________________________________________________________");
