316c2af69cb515bde4baaca8e985addeb7eca0ce
Foysal Ahmed <e1333986@u.nus.edu>
2026-01-25 18:21:33 +0800

    Implement an initial skeletal version

--- src/main/java/FoyBot.java
+++ src/main/java/FoyBot.java
@@ -1,10 +1,11 @@
 public class FoyBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("""
+                ____________________________________________________________
+                Hello! I'm FoyBot!
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________""");
     }
 }
