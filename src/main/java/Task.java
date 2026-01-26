27b987c7e83a61c182b7a20d6e294f59939358ad
Foysal Ahmed <e1333986@u.nus.edu>
2026-01-26 19:54:33 +0800

    Created Task Class

---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "[X]" : "[ ]");
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUndone() {
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + this.description;
+    }
+    //...
+}
