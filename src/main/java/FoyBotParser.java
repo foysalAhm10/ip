db7c287727b88d3b7cfa3de4c8043ebef07caa38
Foysal Ahmed <e1333986@u.nus.edu>
2026-01-30 10:40:43 +0800

    Create FoyBotParser

---
+++ src/main/java/FoyBotParser.java
@@ -0,0 +1,59 @@
+public class FoyBotParser {
+
+    public Instruction parse(String input) throws FoyBotException {
+        input = input.trim();
+
+        if (input.isEmpty()) {
+            throw new FoyBotException("No task found. Please let me do something for you!");
+        }
+
+        String[] parts = input.split(" ", 2);
+        String keyword = parts[0];
+        String rest = (parts.length < 2) ? "" : parts[1].trim();
+
+        switch (keyword) {
+            case "bye":
+                return new ByeInstruction();
+
+            case "todo":
+                if (rest.isEmpty()) {
+                    throw new FoyBotException("todo...what exactly? No todo task description found!");
+                }
+                return new TodoInstruction(rest);
+
+            case "deadline":
+                if (rest.isEmpty()) {
+                    throw new FoyBotException("deadline or no-line? No deadline task description found!");
+                }
+                return new DeadlineInstruction(rest);
+
+            case "event":
+                if (rest.isEmpty()) {
+                    throw new FoyBotException("am i not invited? No event task description found!");
+                }
+                return new EventInstruction(rest);
+
+            case "list":
+                if (!rest.isEmpty()) {
+                    throw new FoyBotException("im confused! To list, just type: list.");
+                }
+                return new ListInstruction();
+
+            case "mark":
+                if (rest.isEmpty()) {
+                    throw new FoyBotException("im confused! State which task to mark.");
+                }
+                return new MarkInstruction(rest);
+
+            case "unmark":
+                if (rest.isEmpty()) {
+                    throw new FoyBotException("im confused! State which task to unmark.");
+                }
+                return new UnmarkInstruction(rest);
+
+            default:
+                throw new FoyBotException("OOPS!!! I don't understand this instruction yet :-(");
+        }
+    }
+}
+
